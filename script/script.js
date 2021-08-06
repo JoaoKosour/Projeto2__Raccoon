96a91bb1a2f1d67c3096a39ab9e87a88eef86cd4
JoaoKosour <joaokosour@gmail.com>
2021-08-06 08:19:48 -0300

    modal desktop completo

--- script/script.js
+++ script/script.js
@@ -1,14 +1,29 @@
-let expand = Array.from(document.querySelectorAll(".competencia__box"));
+const expand = Array.from(document.querySelectorAll(".competencia__box"));
+const modal = document.getElementsByClassName("modal")[0];
+const btn = Array.from(document.querySelectorAll(".button-interesse"));
+const span = document.getElementById("close");
 
-expand.forEach((node) => {
-    node.addEventListener("click", (e) => {
+expand.forEach((e) => {
+    e.addEventListener("click", (e) => {
         //se o elemento atual já esta ativo e é clicado "desativamos" ele.
         if (e.currentTarget.classList.contains("active"))
             e.currentTarget.classList.remove("active");
         //se o elemento atual nao esta ativo, desativa todos os outros elementos e ativa ele
         else {
-            expand.forEach((node) => node.classList.remove("active"));
+            expand.forEach((e) => e.classList.remove("active"));
             e.currentTarget.classList.add("active");
         }
     });
 });
+
+btn.forEach((e) =>
+    e.addEventListener("click", () => (modal.style.display = "flex"))
+);
+
+span.onclick = () => (modal.style.display = "none");
+
+window.onclick = (event) => {
+    if (event.target.classList.contains("modal")) {
+        modal.style.display = "none";
+    }
+};
